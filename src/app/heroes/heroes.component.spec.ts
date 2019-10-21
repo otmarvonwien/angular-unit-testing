ac3ed7e5d9072c61d2d7e318c1441063d8016e25
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-10-21 06:17:37 +0200

    testing a component

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,35 @@
+import { HeroesComponent } from './heroes.component';
+import { Hero } from '../hero';
+// tslint:disable-next-line:import-blacklist
+import { of } from 'rxjs';
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES: Hero[];
+  let mockHeroService: any;
+
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: 'SpiderDude', strength: 8},
+      {id: 2, name: 'Wonderful Woman', strength: 24},
+      {id: 3, name: 'SuperDude', strength: 55}
+    ];
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    component = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete', () => {
+    it('should remove the indicated hero from the heroes list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toEqual(2);
+      expect(component.heroes[0]).toEqual(HEROES[0]);
+      expect(component.heroes[1]).toEqual(HEROES[1]);
+    });
+  });
+});
