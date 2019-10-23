c6107357bfcf380e831834d2746e86decd252237
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-10-23 09:06:47 +0200

    deep integration test

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -1,25 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
-import { NO_ERRORS_SCHEMA, Input, Component } from '@angular/core';
 import { HeroService } from '../hero.service';
+import { HeroComponent } from '../hero/hero.component';
+import { Hero } from '../hero';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 // tslint:disable-next-line:import-blacklist
 import { of } from 'rxjs';
-import { Hero } from '../hero';
 import { By } from '@angular/platform-browser';
 
-describe('HeroesComponent (shallow tests)', () => {
+describe('HeroesComponent (deep tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
-  let mockHeroService;
-  let HEROES;
-
-  @Component({
-    selector: 'app-hero',
-    template: '<div></div>'
-  })
-  class FakeHeroComponent {
-    @Input() hero: Hero;
-    // @Output() delete = new EventEmitter();
-  }
+  let mockHeroService: any;
+  let HEROES: Hero[];
 
   beforeEach(() => {
     HEROES = [
@@ -32,27 +24,26 @@ describe('HeroesComponent (shallow tests)', () => {
     TestBed.configureTestingModule({
       declarations: [
         HeroesComponent,
-        FakeHeroComponent
+        HeroComponent
       ],
       providers: [
         { provide: HeroService, useValue: mockHeroService }
       ],
-      // schemas: [ NO_ERRORS_SCHEMA ]
+      schemas: [ NO_ERRORS_SCHEMA ]
     });
     fixture = TestBed.createComponent(HeroesComponent);
   });
 
-  it('should set heroes correctly from the service', () => {
+  it('should render each hero as a HeroComponent', () => {
     mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    // run ngOnInit
     fixture.detectChanges();
 
-    expect(fixture.componentInstance.heroes.length).toBe(3);
-  });
-
-  it('should create one li for each hero', () => {
-    mockHeroService.getHeroes.and.returnValue(of(HEROES));
-    fixture.detectChanges();
-
-    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+    const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    expect(heroComponentDEs.length).toEqual(3);
+    for (let i = 0; i < heroComponentDEs.length; i++) {
+      expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
+    }
   });
 });
